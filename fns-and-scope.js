1339d6530dfb6e25a3785a3dc80c5aeeaee5afbb
Talon Hughes <talon.hughes@gmail.com>
2016-08-27 10:23:01 -0600

    finished questions

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -5,7 +5,12 @@ var name = 'Tyler';
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 
   //Code Here
-
+function isTyler(name) {
+  if(name === 'Tyler') {
+    return true;
+  }
+  return false;
+}
 //////////////////PROBLEM 2////////////////////
 
 
@@ -13,7 +18,9 @@ var name = 'Tyler';
 
 
   //Code Here
-
+  function getName() {
+    return prompt('What is your name?');
+  }
 
 //////////////////PROBLEM 3////////////////////
 
@@ -23,7 +30,10 @@ var name = 'Tyler';
 //then alerts "Welcome, " plus whatever the users name is.
 
   //Code Here
-
+  function welcome() {
+    var name = getName();
+    alert('Welcome, ' + name);
+  }
 
 //////////////////PROBLEM 4////////////////////
 
@@ -43,7 +53,9 @@ var name = 'Tyler';
 
 
   //Answer Here
-
+ /*
+ 0 , Null , undefined , NaN , false , ""
+ */
 
 
 //////////////////PROBLEM 6////////////////////
@@ -53,15 +65,17 @@ var name = 'Tyler';
 //Create a function called myName that returns your name
 
   //Code Here
-  
+  function myName() {
+    return 'Talon';
+  }
 
 
 //Now save the function definition of myName into a new variable called newMyName
 
   //Code Here
-
+  var newMyName = myName;
 //Now alert the result of invoking newMyName
-
+  alert(newMyName());
 
 
 //////////////////PROBLEM 7////////////////////
@@ -71,9 +85,14 @@ var name = 'Tyler';
 //Create a function called outerFn which returns an anonymous function which returns your name.
 
   //Code Here
-
+function outerFn() {
+  return function() {
+    return 'Talon';
+  }
+}
 //Now save the result of invoking outerFn into a variable called innerFn.
 
   //Code Here
-
+  var innerFn = outerFn();
 //Now invoke innerFn.
+  innerFn();
